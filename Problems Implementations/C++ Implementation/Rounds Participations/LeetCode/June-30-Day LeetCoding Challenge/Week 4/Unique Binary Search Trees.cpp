6a3f17ee1da1cce15d7c5583ee24c9b46496434f
AmEr Hosni <amerhosni07@gmail.com>
2020-06-24 21:04:35 +0100

    Week 4 Day 3

---
+++ Problems Implementations/C++ Implementation/Rounds Participations/LeetCode/June-30-Day LeetCoding Challenge/Week 4/Unique Binary Search Trees.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        std::vector<int> dp(n+1);
+        dp[0]=dp[1]=1;
+
+        for (int i = 2; i <= n; i++) { 
+	        for (int j = 1; j <= i; j++) { 
+	            dp[i] = dp[i] + (dp[i - j] * dp[j - 1]); 
+	        } 
+    	} 
+  
+    	return dp[n];
+    }
+};
