10cadba9430014226c388493c57931cd5ab5e08a
AmEr Hosni <amerhosni07@gmail.com>
2020-06-29 11:35:25 +0100

    Uva problems

---
+++ Problems Implementations/C++ Implementation/Upsolving/UVA/CP3/Introduction/The easy problems/Super Easy/11547 - Automatic Answer.cpp
@@ -0,0 +1,36 @@
+//                             ¯\_(ツ)_/¯   
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mma,ava,tune=native")
+//#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define sz(a) ((int)(a).size())
+#define all(a) (a).begin(),(a).end()
+#define ll long long
+ 
+ 
+void file(){
+        #ifndef ONLINE_JUDGE
+        freopen("test.txt", "r", stdin);
+        #endif
+}
+int tc;
+ 
+const int N=2e5+5,M=2e6+5,MOD=1e9+7,OO=1e9;
+
+int main(){
+        ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+        file();
+        
+        scanf("%d",&tc);
+
+        while(tc--){
+            int n;
+            scanf("%d",&n);
+            printf("%d\n",abs((((n*567/9 + 7492) * 235 / 47 - 498)%100)/10));
+        }
+        
+        
+}
---
+++ Problems Implementations/C++ Implementation/Upsolving/UVA/CP3/Introduction/The easy problems/Super Easy/11727 - Cost Cutting.cpp
@@ -0,0 +1,38 @@
+//                             ¯\_(ツ)_/¯   
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mma,ava,tune=native")
+//#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define sz(a) ((int)(a).size())
+#define all(a) (a).begin(),(a).end()
+#define ll long long
+ 
+ 
+void file(){
+        #ifndef ONLINE_JUDGE
+        freopen("test.txt", "r", stdin);
+        #endif
+}
+int tc;
+ 
+const int N=2e5+5,M=2e6+5,MOD=1e9+7,OO=1e9;
+
+int main(){
+        ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+        file();
+        
+        scanf("%d",&tc);
+
+        for(int i=1;i<=tc;i++){
+            printf("Case %d: ",i);
+            std::vector<int> a(3);
+            scanf("%d %d %d",&a[0],&a[1],&a[2]);
+            sort(all(a));
+            printf("%d\n",a[1]);
+        }
+        
+        
+}
