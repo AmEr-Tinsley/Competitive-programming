1f3fb0d152821a572d5c9daa8e76c9af97b8a7a3
unknown <amerhosni07@gmail.com>
2020-10-31 16:14:37 +0100

    CF upsolve

---
+++ Problems Implementations/C++ Implementation/Upsolving/Codeforces/D. Districts Connection.cpp
@@ -0,0 +1,31 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+//#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+ 
+#define pb push_bac(X/j)
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(),(x).end()
+#define ll long long
+ 
+using namespace std;
+ 
+void file(){
+	#ifndef ONLINE_JUDGE
+    	freopen("in.txt", "r", stdin);
+	#endif
+}
+int tc;
+const int N=2e5+5,M=2e6+5,MOD=998244353,OO=1e9;
+
+void solve(){
+	
+}
+int main(){
+	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	file();
+ 
+	scanf("%d",&tc);
+
+	while(tc--)solve();
+}
