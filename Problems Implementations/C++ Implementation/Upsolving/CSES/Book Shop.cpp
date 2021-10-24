371da2312771ec134af22409f408108947bce5d0
AmEr-Tinsley <amerhosni07@gmail.com>
2021-10-24 22:09:34 +0200

    book shop dp

---
+++ Problems Implementations/C++ Implementation/Upsolving/CSES/Book Shop.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/detail/standard_policies.hpp>
+#define pb push_back
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(),(x).end()
+#define ll long long
+using namespace std;
+//using namespace  __gnu_pbds;
+
+//typedef tree< int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+void file(){
+	#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    	//freopen("out.txt","w",stdout);
+	#endif
+}
+int tc;
+const int N=3e5+5,M=2e6+5,MOD=1e9+7,OO=1e9+7;
+const ll INF = 2e18;
+
+
+void solve(int tc ){
+    int n,x;
+
+    scanf("%d %d",&n,&x);
+
+    vector<int>a(n),b(n);
+
+    for(int i =0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+
+    for(int i =0;i<n;i++){
+        scanf("%d",&b[i]);
+    }
+
+    vector<int>dp(x+1),dp2(x+1);
+
+    for(int i =0;i<n;i++){
+        for(int j = a[i];j<=x;j++){
+            dp[j] = max(dp[j],dp2[j-a[i]] + b[i]);
+            dp[j] = max(dp[j],dp[j-1]);
+        }
+        dp2 = dp;
+    }
+
+    printf("%d\n",dp[x]);
+
+}
+int main(){
+	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	file();
+
+    int tc = 1;
+ 	//scanf("%d",&tc);
+    for(int i = 1;i<=tc;i++)solve(i);
+}
