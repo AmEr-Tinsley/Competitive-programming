6e82dc1ee3e4bbf3644e58680ed8ab625fac5933
AmEr-Tinsley <amerhosni07@gmail.com>
2021-12-03 13:14:16 +0100

    Advent of code Day 1 + 2

---
+++ Problems Implementations/C++ Implementation/Rounds Participations/Advent of code/2021/Day 1_part 2.cpp
@@ -0,0 +1,55 @@
+//                             ¯\_(ツ)_/¯  
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/detail/standard_policies.hpp>
+#define pb push_back
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define ll long long
+using namespace std;
+//using namespace  __gnu_pbds;
+
+//typedef tree< int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+void file()
+{
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    //freopen("out.txt","w",stdout);
+#endif
+}
+int tc;
+const int N = 1e6 + 5, M = 2e6 + 5, MOD = 1e9 + 7, OO = 1e9 + 7;
+const ll INF = 2e18;
+
+void solve(int tc)
+{
+    vector<int>a;
+    int x;
+    while(cin>>x){
+        a.pb(x);
+    }
+    int n = sz(a), ans = 0;
+    int last = a[0] + a[1] + a[2];
+    for(int i = 1;i+2<n;i++){
+        int curr = a[i] + a[i+1] + a[i+2];
+        ans+=(curr > last);
+        last = curr;
+    }
+
+    cout<<ans<<endl;
+    
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    file();
+ 
+    int tc = 1;
+    //scanf("%d",&tc);
+    for (int i = 1; i <= tc; i++)
+        solve(i);
+}
